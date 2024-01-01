3f66f187a397a06bb8a2af66762a406d20ecea5f
syske <715448004@qq.com>
2024-01-01 13:18:16 +0800

    update code

---
+++ example-01/example01.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"syske.github.io/example-01/pkg1"
+)
+
+func main() {
+	pkg1.Pkg1()
+
+}
---
+++ example-01/pkg1/constant_test.go
@@ -0,0 +1,14 @@
+package pkg1
+
+import "fmt"
+
+const (
+	pi   = 3.14
+	Name = "pkg1"
+)
+
+func Pkg1() {
+	fmt.Print("pkg1")
+	fmt.Print(Name)
+	fmt.Print(pi)
+}
